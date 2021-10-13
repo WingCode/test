a5004061c1c8be6a1117510228fd1a1d4902fad0
WingCode <smallstar1234@gmail.com>
2021-10-14 01:14:12 +0530

    Update testPOST.js

--- testPOST.js
+++ testPOST.js
@@ -1,6 +1,14 @@
-function testinPOST(myData){
-fetch("https://test115.free.beeceptor.com", {
-  method: "POST",
-  body: JSON.stringify(myData)
-});
-}
+var sendPOST = (function () {
+    function sendPOST() {
+    }
+    sendPOST.sendEvent = function (payload) {
+        fetch("https://test115.free.beeceptor.com", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+        });
+    };
+    return sendPOST;
+}());
